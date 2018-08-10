b2675b837300d5fce5d5e1a34d05a9b30cd44d94
Dioni Mercado <me@dionimercado.com>
2018-08-10 15:33:42 -0400

    Added footer

--- footer.php
+++ footer.php
@@ -2,10 +2,11 @@
   <div class="container">
     <div class="row">
       <div class="col-md-7">
-        <p>&copy; <?php echo date("Y") ?> <strong>Paneldom</strong>. Todos los derechos reservados.</p>
+        <p>&copy; <?php echo date( 'Y' ) ?> <strong>Paneldom</strong>. Todos los derechos reservados.</p>
       </div>
       <div class="col-md-5">
         <div class="social-icons text-right">
+          <span><?php _e( 'Síguenos' ) ?></span>
           <a href="https://www.facebook.com/paneldomrd" target="_blank"><i class="fab fa-facebook-f"></i></a>
           <a href="https://www.instagram.com/paneldomrd" target="_blank"><i class="fab fa-instagram"></i></a>
           <a href="https://www.twitter.com/paneldomrd" target="_blank"><i class="fab fa-twitter"></i></a>

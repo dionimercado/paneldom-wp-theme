706cadae5149857ebb06e6a1898ed466f38c9716
Dioni Mercado <me@dionimercado.com>
2018-08-27 23:26:43 -0400

    Footer improvements

--- footer.php
+++ footer.php
@@ -1,20 +1,51 @@
 <footer class="footer">
   <div class="container">
     <div class="row">
-      <div class="col-md-7">
-        <p>&copy; <?php echo date( 'Y' ) ?> <strong>Paneldom</strong>. Todos los derechos reservados.</p>
+      <div class="col-md-4">
+        <h2>Horario Laboral</h2>
+        <ul>
+          <li><i class="far fa-calendar-alt"></i> <strong>Lunes - Viernes:</strong><br /><i class="far fa-clock"></i> 8:00 am 12:00pm | 1:00 - 6:00 pm</li>
+          <li><i class="far fa-calendar-alt"></i> <strong>S&aacute;bados:</strong><br /><i class="far fa-clock"></i> 8:00 am - 12.00 pm</li>
+          <li><i class="far fa-calendar-alt"></i> <strong>Domingos:</strong><br /><i class="far fa-clock"></i> cerrado</li>
+        </ul>
       </div>
-      <div class="col-md-5">
-        <div class="social-icons text-right">
-          <span><?php _e( 'Síguenos' ) ?></span>
-          <a href="https://www.facebook.com/paneldomrd" target="_blank"><i class="fab fa-facebook-f"></i></a>
-          <a href="https://www.instagram.com/paneldomrd" target="_blank"><i class="fab fa-instagram"></i></a>
-          <a href="https://www.twitter.com/paneldomrd" target="_blank"><i class="fab fa-twitter"></i></a>
+      <div class="col-md-4">
+        <h2>Contactos</h2>
+        <ul>
+          <li>
+            <i class="fas fa-map-marker-alt"></i> Carretera Peña Km. 1 #21, Licey al medio<br />
+            Santiago, República Dominicana</li>
+            <li><i class="fas fa-phone"></i> (809) 736 - 9042</li>
+            <li><i class="fas fa-phone"></i> (809) 736 - 2722</li>
+            <li><i class="fas fa-phone"></i> (809) 736 – 2664</li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <!-- <h2>C&oacute;mo Llegar</h2> -->
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44870.65007564818!2d-70.65626702200821!3d19.463057278018812!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eb1c554bc0dba73%3A0x2d7158dea511e119!2sPaneldom!5e0!3m2!1sen!2sus!4v1535413663871" width="100%" height="170" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
       </div>
     </div>
-  </div>
-</footer>
-<?php wp_footer() ?>
+    <div class="footer-bottom">
+
+      <div class="container">
+        <div class="row">
+          <div class="col-md-7">
+            <p>&copy; <?php echo date( 'Y' ) ?> <strong>Paneldom</strong>. Todos los derechos reservados.</p>
+          </div>
+          <div class="col-md-5">
+            <div class="social-icons text-right">
+              <span><?php _e( 'Síguenos' ) ?></span>
+              <a href="https://www.facebook.com/paneldomrd" target="_blank"><i class="fab fa-facebook-f"></i></a>
+              <a href="https://www.instagram.com/paneldomrd" target="_blank"><i class="fab fa-instagram"></i></a>
+              <a href="https://www.twitter.com/paneldomrd" target="_blank"><i class="fab fa-twitter"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </footer>
+  <?php wp_footer() ?>
 </body>
 </html>
